52409ff3d1db00b20c963699b5c0d4f143e88815
Carlos Henao <carlos.henaog@udea.edu.co>
2022-03-23 01:32:00 -0500

    (feat) using next api as data end point

--- src/hooks/useGetPlaces.js
+++ src/hooks/useGetPlaces.js
@@ -2,12 +2,13 @@ import axios from "axios";
 import { useQuery } from "react-query";
 
 
-const API = process.env.PLACES_APP_API || "http://localhost:3005";
+const API = process.env.PLACES_APP_API || "http://localhost:3000/api";
 
 
 const getPlaces = () =>
   axios.get(`${API}/places`)
     .then(response => response.data)
+
 
 export default function useGetPlaces() {
   return useQuery(["places"], () => getPlaces());
